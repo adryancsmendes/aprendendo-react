577bdf8fcc0e0756fbf9bb6a29d7ba8697210bf6
Adryan Mendes <adryanmendes2014@gmail.com>
2023-03-02 13:54:18 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
+  const name = "Adryan";
+  const newName = name.toUpperCase();
+
+  function sum(a,b) {
+    return a + b
+  }
+
+  const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Alterando o JSX</h2>
+      <p>Olá, {newName}</p>
+      <p>Soma: {sum(1,2)}</p>
+      <img src={url} alt="minha imagem" />
+      <HelloWorld />
     </div>
   );
 }
