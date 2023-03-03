16d83c4100347898b49e99eea696a9e46f0d5e3c
Adryan Mendes <adryanmendes2014@gmail.com>
2023-03-03 19:51:36 -0300

    aprendendo props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import './App.css';
 import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
-  const name = "Adryan";
-  const newName = name.toUpperCase();
-
-  function sum(a,b) {
-    return a + b
-  }
-
-  const url = 'https://via.placeholder.com/150'
+  const nome = "Maria"
 
   return (
     <div className="App">
-      <h2>Alterando o JSX</h2>
-      <p>Olá, {newName}</p>
-      <p>Soma: {sum(1,2)}</p>
-      <img src={url} alt="minha imagem" />
-      <HelloWorld />
+      <SayMyName nome="Matheus" />
+      <SayMyName nome="João" />
+      <SayMyName nome={nome} />
+      <Pessoa
+        nome="Rodrigo"
+        idade="28"
+        profissao="Programador"
+        foto="https://via.placeholder.com/150"
+      />
     </div>
   );
 }
