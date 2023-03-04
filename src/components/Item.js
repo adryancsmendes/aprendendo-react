2a854b537924a7b915f6b65a56307f75662afe0d
Adryan Mendes <adryanmendes2014@gmail.com>
2023-03-04 10:09:06 -0300

    avançando em props

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,9 +1,21 @@
-function Item(props) {
+import PropTypes from 'prop-types'
+
+function Item({marca, ano_lançamento}) {
     return (
         <>
-            <li>{props.marca}</li>
+            <li>{marca} - {ano_lançamento}</li>
         </>
     )
+}
+
+Item.propTypes = {
+    marca: PropTypes.string.isRequired,
+    ano_lançamento: PropTypes.number,
+}
+
+Item.defaultProps = {
+    marca: 'Faltou a marca',
+    ano_lançamento: 0,
 }
 
 export default Item
--- src/components/List.js
+++ src/components/List.js
@@ -5,9 +5,11 @@ function List() {
         <>
             <h1>Minha Lista</h1>
             <ul>
-                <Item marca="Ferrari"/>
-                <Item marca="Fiat"/>
+                <Item marca="Ferrari" ano_lançamento={1985}/>
+                <Item marca="Fiat" ano_lançamento={1964}/>
                 <Item marca="Renault"/>
+                <Item marca="Crevrolet" ano_lançamento={1999}/>
+                <Item/>
             </ul>
         </>
     )
