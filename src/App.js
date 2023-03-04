3d79314a13b3a0abc3f739674eca8962c3041b1c
Adryan Mendes <adryanmendes2014@gmail.com>
2023-03-04 09:38:22 -0300

    aprendendo react fragments

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
 import Frase from './components/Frase'
+import List from './components/List'
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
         profissao="Programador"
         foto="https://via.placeholder.com/150"
       />
+      <List />
     </div>
   );
 }
